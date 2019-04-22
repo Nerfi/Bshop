19082c9234af0889e27c9c9de8694c53f67b8cec
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-04-22 18:57:51 +0200

    workign display cl img

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,10 +1,10 @@
 class Shop < ApplicationRecord
   belongs_to :user
+  #adding mount_uploader for cloudinary
+   mount_uploader :photo, PhotoUploader
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true, length: { minimum: 15 }
   validates :price, presence: true
 
-  #adding mount_uploader for cloudinary
-   mount_uploader :photo, PhotoUploader
 end

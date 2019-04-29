d25135696af4c4b278a699b1839763af73f0f6bd
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-04-29 21:35:35 +0200

    starting rating system

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_26_184801) do
+ActiveRecord::Schema.define(version: 2019_04_29_193147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_04_26_184801) do
     t.bigint "shop_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
     t.index ["shop_id"], name: "index_reviews_on_shop_id"
   end
 

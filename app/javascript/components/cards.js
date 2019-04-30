6a935c371b3e63c6a513b60f0f4ac61501e98448
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-04-30 19:42:01 +0200

    working  event listener home page

--- app/javascript/components/cards.js
+++ app/javascript/components/cards.js
@@ -1,3 +1,68 @@
 const displayContent = () => {
-const parraf = document.getElementById(`#parraf`);
+  const parraf = document.getElementById('parraf');
+  parraf.addEventListener("click", (event) => {
+  event.preventDefault();
+  //parraf.classList.add("demo");
+  parraf.insertAdjacentHTML("beforeend", "<h2>Search over all the Bshops created by others users</h2>")
+
+
+
+  })
 }
+
+
+const findContent = () => {
+  const parraf = document.getElementById('find');
+  parraf.addEventListener("click", (event) => {
+  event.preventDefault();
+  //parraf.classList.add("demo");
+  parraf.insertAdjacentHTML("beforeend", "<h2>Search over all the Bshops created by others users</h2>")
+
+
+
+  })
+}
+
+
+const bookContent = () => {
+  const parraf = document.getElementById('book');
+  parraf.addEventListener("click", (event) => {
+  event.preventDefault();
+  //parraf.classList.add("demo");
+  parraf.insertAdjacentHTML("beforeend", "<h2>Search over all the Bshops created by others users</h2>")
+
+
+
+  })
+}
+
+
+
+const shineContent = () => {
+  const parraf = document.getElementById('shine');
+  parraf.addEventListener("click", (event) => {
+  event.preventDefault();
+  //parraf.classList.add("demo");
+  parraf.insertAdjacentHTML("beforeend", "<h2>Search over all the Bshops created by others users</h2>")
+
+
+
+  })
+}
+
+
+export { shineContent };
+
+export { bookContent };
+
+
+
+
+export { findContent };
+
+
+
+
+
+export { displayContent };
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,18 @@ loadDynamicBannerText ();
 
 import { initStarRating } from '../plugins/init_star_rating';
 initStarRating();
+
+
+import { displayContent } from '../components/cards';
+displayContent ();
+
+
+import { findContent } from '../components/cards';
+findContent ();
+
+import { bookContent } from '../components/cards';
+bookContent ();
+
+
+import { shineContent } from '../components/cards';
+shineContent ();

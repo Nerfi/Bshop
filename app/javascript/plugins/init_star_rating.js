efd00785595fc5b75ff5deb08d1fa521a1a73d47
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-04-30 14:42:02 +0200

    apparently its working the rating system, but seems to work , cool

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -8,7 +8,12 @@ import "jquery-bar-rating/dist/themes/css-stars.css";
 
 const initStarRating = () => {
   $('#shop_review_rating').barrating({
-    theme: 'css-stars'
+    theme: 'css-stars',
+    showSelectedRating: true,
+    onSelect: (value, text, event) => {
+      const form = $("form.review_form");
+      form.submit();
+    }
   });
 };
 

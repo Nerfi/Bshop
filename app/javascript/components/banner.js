fc767b1593249bba2764564617389f0596b7c541
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-04-30 13:07:47 +0200

    working rating

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,25 @@
 import Typed from 'typed.js';
 
-const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Look cool", "around the world"],
-    typeSpeed: 50,
-    loop: true
-  });
-}
+//const loadDynamicBannerText = () => {
+  //new Typed('#banner-typed-text', {
+    //strings: ["Look cool", "around the world"],
+    //typeSpeed: 50,
+    //loop: true
+  //});
+//}
 
 export { loadDynamicBannerText }
+
+
+// nuevo codigo añadido para no tener mas el error de Type on the console
+
+const loadDynamicBannerText = () => {
+  const banner = document.getElementById('banner-typed-text');
+  if (banner) {
+    new Typed('#banner-typed-text', {
+      strings: ["Look cool", "around the world"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
+}

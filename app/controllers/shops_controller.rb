ac0de19712f32b24966b858b938e37e945b9ee8e
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-05-01 20:33:02 +0200

    not showing google maps

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,7 +4,7 @@ class ShopsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    #@shops =Shop.all
+
     @shops = policy_scope(Shop).order(created_at: :desc)
     @shops = Shop.where.not(latitude: nil, longitude: nil)
 

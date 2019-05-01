082d3dfbdd5ed9f3c51509948e719c01260a8ea9
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-05-01 19:14:41 +0200

    problem wiht cloudinary path

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -6,6 +6,17 @@ class ShopsController < ApplicationController
   def index
     #@shops =Shop.all
     @shops = policy_scope(Shop).order(created_at: :desc)
+    @shops = Shop.where.not(latitude: nil, longitude: nil)
+
+    @markers = @shops.map do |shop|
+      {
+        lat: shop.latitude,
+        lng: shop.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+        # Uncomment the above line if you want each of your markers to display a info window when clicked
+        # (you will also need to create the partial "/flats/map_box")
+      }
+    end
 
   end
 
